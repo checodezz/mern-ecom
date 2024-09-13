97454ca2c60bc7506dab48166f833fd5c4c8772a
Ashwini Pendkar <ashwinipendkar953@gmail.com>
2024-09-13 16:19:45 +0530

    docs: Update index.html to include custom CORS settings

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,17 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,  Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
+
 app.use("/api", userRouter);
 app.use("/api", adminRouter);
 app.use("/api", productRouter);
